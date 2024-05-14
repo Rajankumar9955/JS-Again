0cc827417cbfa530bead0509fb5a255a5c6419f6
Rajan <krajan92946@gmail.com>
2024-05-14 17:50:36 +0530

    form

--- DOM/form/form.js
+++ DOM/form/form.js
@@ -13,11 +13,22 @@ function validate()
         document.getElementById('namee').focus()
         return false
     }
+    else if(namee.match(/[0,1,2,3,4,5,6,7,9]/)){
+        alert("Name should be in charater")
+        document.getElementById('namee').focus()
+        return false
+    }
     else if(email=="")
     {
         alert("Email is required")
         document.getElementById('email').focus()
         return false;
+    }
+    else if(!(email.includes('@')))
+    {
+        alert("@ is compulsary in email")
+        document.getElementById('email').focus()
+        return false
     }
     else if(contact=="")
     {
@@ -30,6 +41,12 @@ function validate()
         alert("contact in number")
         document.getElementById("contact").focus()
         return false
+    }
+    else if(contact.length>10 || contact.length<10)
+    {
+        alert("Contact should be 10 digit")
+        document.getElementById("contact").focus();
+        return false;
     }
     else if(isNaN(age))
     {
@@ -49,6 +66,12 @@ function validate()
         document.getElementById("pass").focus()
         return false
     }
+    else if(!(pass.match(/[!,@,#,>,<,.]/)))
+    {
+        alert("Special charater is required in password")
+        document.getElementById("pass").focus()
+        return false
+    }
     else if(cpass=="")
     {
         alert("cpass is required")
@@ -58,6 +81,8 @@ function validate()
     else if(pass!=cpass)
     {
         alert("Doesn't match password")
+        document.getElementById("cpass").focus()
+        return false
         
     }
 }
