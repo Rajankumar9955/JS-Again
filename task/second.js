05ff694a89e2a285e358343ca6e219d3cde60adc
Rajan <krajan92946@gmail.com>
2024-05-23 05:18:05 +0530

    button task

---
+++ task/second.js
@@ -0,0 +1,31 @@
+
+let list0=document.querySelector("#list0")
+list0.addEventListener("mouseover",()=>{
+list0.innerHTML="ON";
+list0.style.backgroundColor="red";
+})
+
+let list1=document.querySelector("#list1")
+list1.addEventListener("mouseover",()=>{
+list1.innerHTML="ON";
+list1.style.backgroundColor="red";
+})
+
+let list2=document.querySelector("#list2")
+list2.addEventListener("mouseover",()=>{
+list2.innerHTML="ON";
+list2.style.backgroundColor="red";
+})
+
+let list3=document.querySelector("#list3")
+list3.addEventListener("mouseover",()=>{
+list3.innerHTML="ON";
+list3.style.backgroundColor="red";
+})
+
+let list4=document.querySelector("#list4")
+list4.addEventListener("mouseover",()=>{
+list4.innerHTML="ON";
+list4.style.backgroundColor="red";
+})
+
