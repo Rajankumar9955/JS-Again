04d8f165164742dec7f3f03cd8b9f1ddddc7fc0e
Rajan <krajan92946@gmail.com>
2024-05-24 17:40:58 +0530

    array of object

---
+++ Object/objecttask.js
@@ -0,0 +1,27 @@
+let obj=
+{
+ namee:"Rajan kumar",
+ age:20,
+ post:"Software engineer",
+ contact:9955236973,
+  salary :{
+     annual_income:500000,
+     monthly_Income:45000,
+     leaves:12
+ }
+}
+console.log(obj)
+
+obj.salary.annual=200000;
+console.log(obj)
+
+obj.salary.leaves=6;
+obj.salary.conditional_Leves=10;
+console.log(obj)
+
+obj.post="fullStack Developer";
+console.log(obj);
+
+ delete obj.age
+console.log(obj)
+
---
+++ array of Object/arrOfObject.js
@@ -0,0 +1,32 @@
+let emp=[
+    {
+        namee:"Rajan kumar",
+        age:20,
+        city:"sitamarhi",
+        contact:9955236973,
+        designation:"Junior engineer"
+    },
+    {
+        namee:"suraj kumar",
+        age:21,
+        city:"Bairgania",
+        contact:6206546029,
+        designation:"Junior engineer"
+
+    },
+    {
+        namee:"Hariom kumar",
+        age:30,
+        city:"joriyahi",
+        contact:9955236973,
+        designation:"manager"
+    },
+    {
+        namee:"vikash kumar",
+        age:19,
+        city:"parsauni",
+        contact:9855685688,
+        designation:"student"
+    }
+]
+console.table(emp)
