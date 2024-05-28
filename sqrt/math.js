5553b220a1832d7536c1fbba2da917bd1c94806a
Rajan <krajan92946@gmail.com>
2024-05-28 17:34:37 +0530

    math function

---
+++ sqrt/math.js
@@ -0,0 +1,35 @@
+let a=16;
+console.log(Math.sqrt(a))
+
+
+let val=Math.max(1,25,56,321,52,85)
+console.log(val)
+
+let min=Math.min(5,35,32,54,12,4)
+console.log(min)
+
+
+let p=Math.pow(3,2) // pow function jisme two argument pass krte hai first base and second power
+console.log(p)
+
+
+let val2=123.2585
+let u=Math.trunc(val2)  //decimal value ko ignore krta hai
+console.log(u)
+
+
+let s=Math.random()+1
+console.log(s)
+
+let v=Math.random()*9
+console.log(v)
+
+
+
+let r=Math.random()*9+1
+console.log(r)
+
+
+let t=Math.random()*9+1
+let b=Math.trunc(t)
+console.log(b)
---
+++ sqrt/task.js
@@ -0,0 +1,28 @@
+// function fun()
+// {
+//     let a=document.getElementById("mains")
+//     let val=Math.random()*10+1
+//     let r=Math.trunc(val)
+//     a.innerHTML=r;
+   
+
+
+//     let b=document.getElementById("main")
+//     let val2=Math.random()*10+1
+//     let re=Math.trunc(val2)
+//     b.innerHTML=re
+
+// }
+
+
+
+function fun()
+{
+    let a=document.getElementById("mains")
+    let b=document.getElementById("main")
+    a.innerHTML = Math.trunc(Math.random()*10+1)
+    b.innerHTML = Math.trunc(Math.random()*10+1)
+}
+
+let s= "hello"
+console.log(s[0])
