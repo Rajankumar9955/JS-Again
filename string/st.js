e75bec8f3683d20afb9670f9a4d99f1ba659d134
Rajan <krajan92946@gmail.com>
2024-05-29 17:02:39 +0530

    string

---
+++ string/st.js
@@ -0,0 +1,28 @@
+let s="Rajan "
+let st=s.toUpperCase()
+console.log(st)
+
+
+
+let st1 =s.toLowerCase()
+console.log(st1)
+
+let rep=s.repeat(10)
+console.log(rep)
+
+
+let sub=s.substring(0,5) //starting index and ending index
+console.log(sub)
+
+
+let ch=s.charAt(4)
+console.log(ch)
+
+
+
+//string ke or v function hote hai explore kr sakte hai
+
+
+
+
+//match and include dono string ka part hai jo hum form validation mein padhe hai
---
+++ string/tamp.js
@@ -0,0 +1,10 @@
+let s="hello...\n this is  kumar"
+
+
+
+// (``):- Template literal 
+
+let n="rajan"
+let s1=`Hello this is ${n} kumar
+the engineer`
+console.log(s1)
