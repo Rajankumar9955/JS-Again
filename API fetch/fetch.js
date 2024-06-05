33c71ba51aafa6edbaba6375071717a70567d54a
Rajan <krajan92946@gmail.com>
2024-06-05 17:58:32 +0530

    Api post

---
+++ API fetch/fetch.js
@@ -0,0 +1,34 @@
+async function fun()
+{   let Data = document.getElementById("Data")
+    let a= await fetch("http://localhost:3000/product")
+    let b= await a.json()
+    
+    let d=b.map((e)=>`
+    <tr>
+          <td>${e.id}</td>
+          <td>${e.pr_name}</td>
+          <td>${e.price}</td>
+    </tr>
+    `).join("")
+    Data.innerHTML=d
+}
+
+
+function add()
+{
+    let frmdata = {
+        id:document.getElementById("n1").value,
+        pr_name:document.getElementById("n2").value,
+        price:document.getElementById("n3").value
+
+    }
+
+    fetch("",{
+        method:"post",
+        headers:{
+            'Content-type':'application/json',
+        },
+        body:JSON.stringify(frmdata)
+    })
+}
+
