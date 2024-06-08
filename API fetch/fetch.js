6bd159d9ce53ee994e75a82847fef593efe1613e
Rajan <krajan92946@gmail.com>
2024-06-08 22:45:34 +0530

    API Edit

--- API fetch/fetch.js
+++ API fetch/fetch.js
@@ -5,16 +5,19 @@ async function fun()
     
     let d=b.map((e)=>`
     <tr>
-          <td>${e.id}</td>
+          <td>${e.id}</td> 
           <td>${e.pr_name}</td>
           <td>${e.price}</td>
           <td><button onclick="mydelete(${e.id})">Delete</buttton></td>
+          <td><button onclick="FormEdit(${e.id})">Edit</buttton></td> 
     </tr>
     `).join("")
     Data.innerHTML=d
+    // td<button onclick="mydelete{${e.id}"> this is use for delete section
+    // td<button onclick="FormEdit{${e.id}"> this is use for edit section
 }
 fun()
-
+//==================add section==================
 function add()
 {
     let frmdata = {
@@ -32,9 +35,42 @@ function add()
         body:JSON.stringify(frmdata)
     })
 }
+//=====================delete section==============
+
+
 function mydelete(id){
     fetch(`http://localhost:3000/product/${id}`,{
         method:"Delete",
     })
     .then(res=>alert("Data Deleted Successfully"))
+}
+
+
+//===========edit section=======================
+
+
+let Raj=0;
+function FormEdit(id)
+{
+    Raj=id;
+    document.getElementById("frmdata").style.display="block";
+}
+
+function EditForm()
+{
+         let edit = {
+             id:document.getElementById("id").value,
+             pr_name:document.getElementById("pr_name").value,
+             price:document.getElementById("price").value,
+         }
+
+         fetch(`http://localhost:3000/product/${Raj}`,{
+            method:"PUT",
+            headers:{
+                'Content-type':'application/json',
+           },
+           body:JSON.stringify(edit),
+         })
+         .then(res=>alert("Edit Done"))
+  
 }
