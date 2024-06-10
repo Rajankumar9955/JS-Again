11785ab4e78eb742e1bf5c27e61ea2f771370353
Rajan <krajan92946@gmail.com>
2024-06-10 17:32:52 +0530

    jquiery

---
+++ jquiery/index.js
@@ -0,0 +1,11 @@
+//left side mein selector 
+// right side mein function
+
+$(document).ready(function(){
+    let a = $('#head').html()
+    console.log(a);
+
+    $('#btn').click(function(){
+        alert("clicked")
+    })
+})
---
+++ jquiery/task.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    // let a = $('#main').html()
+
+    let b = $('.heading').html()
+    console.log(b);
+
+    let c = $("#para").html()
+    console.log(c)
+
+    $('button').dblclick(function(){
+    alert("Clicked")
+    console.log("Double Clicked")
+    })
+   
+})
