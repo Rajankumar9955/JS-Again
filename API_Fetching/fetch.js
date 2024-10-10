a06bf4c65cc5034520a70b3b2b1da2f16d755404
Rajan <krajan92946@gmail.com>
2024-10-10 08:07:13 +0530

    synchrounus

--- API_Fetching/fetch.js
+++ API_Fetching/fetch.js
@@ -1,6 +1,6 @@
 //node js is a run time environment to provide environment jo multiple language ko support krta hai :- js , next js etc
 //sync await promise return krta hai or promise ek object hota hai
-//map function fetch krta hai or new function return krt hai
+//map function fetch krta hai or new function return krta hai
 //fetch function data fetch krta hai 
 
 
